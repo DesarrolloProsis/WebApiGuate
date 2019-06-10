57354e33497c3865f978b8d0c810b137039699e5
GerardoLucas <soreclucas74@gmail.com>
2019-06-10 17:28:11 -0600

    Consultas probadas en Guate, funcionando

--- WebApiGuate/Controllers/HomeController.cs
+++ WebApiGuate/Controllers/HomeController.cs
@@ -28,21 +28,23 @@ namespace WebApiGuate.Controllers
         {
             return HistoricoListas;
         }
-        public async void GetOracleTransactions()
+        public void GetOracleTransactions()
         {
-            var query = @"Select count(*) From Transaction Where  ID_PAIEMENT = '15'
-                        AND TO_CHAR(DATE_TRANSACTION, 'YYYY/MM/DD HH24:MI:SS' ) >= '2019/03/21 12:31:55' 
-                        AND SUBSTR(TO_CHAR(CONTENU_ISO),0,3) = '501' 
+            var query = @"Select count(*) From Transaction Where  ID_PAIEMENT = '15' 
+                        AND TO_CHAR(DATE_TRANSACTION, 'YYYY/MM/DD HH24:MI:SS' ) >= '2019/03/21 12:31:55'  
+                        AND SUBSTR(TO_CHAR(CONTENU_ISO),0,3) = '501'  
                         AND TAB_ID_CLASSE >= 1 order by DATE_TRANSACTION ASC";
             OracleConnection connection = new OracleConnection("User Id = GEADBA; Password = fgeuorjvne; Data Source = (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 10.1.10.111)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = GEAPROD)))");
+            connection.Open();
             OracleCommand cmd = new OracleCommand(query, connection);
-            OracleTransactions = cmd.ExecuteNonQuery();
+            OracleTransactions = Convert.ToInt32(cmd.ExecuteScalar());
+            connection.Close();
         }
         public int SendOracleTransactions()
         {
             return OracleTransactions;
         }
-        public async void GetSQLTransactions()
+        public void GetSQLTransactions()
         {
             SQLTransactions = db.Historico.Count();
         }

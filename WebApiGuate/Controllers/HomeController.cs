0bfab64138c8d75602bb90d21feacd99d4e5765a
Miguel Ramirez <mike__221@hotmail.com>
2019-06-12 19:35:27 -0500

    Correcciones y cambio de nombre en métodos e inclusión de métodos para Tabla Historico (ServiceTags)

--- WebApiGuate/Controllers/HomeController.cs
+++ WebApiGuate/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace WebApiGuate.Controllers
     {
         GTDBEntities db = new GTDBEntities();
         public static DateTime HistoricoListas { get; set; }
+        public static DateTime HistoricoTags { get; set; }
         public static int OracleTransactions { get; set; }
         public static int SQLTransactions { get; set; }
         public ActionResult Index()
@@ -20,29 +21,39 @@ namespace WebApiGuate.Controllers
 
             return View();
         }
-        public void GetListas()
+        public void GetHistoricoListas()
         {
-            HistoricoListas = db.HistoricoListas.ToList().OrderByDescending(x => x.Fecha_Creacion).FirstOrDefault().Fecha_Creacion;
+            HistoricoListas = db.HistoricoListas.OrderByDescending(x => x.Fecha_Creacion).FirstOrDefault().Fecha_Creacion;
         }
-        public DateTime SendListas()
+        public DateTime SendHistoricoListas()
         {
             return HistoricoListas;
         }
-        public async void GetOracleTransactions()
+        public void GetHistoricoTags()
         {
-            var query = @"Select count(*) From Transaction Where  ID_PAIEMENT = '15'
-                        AND TO_CHAR(DATE_TRANSACTION, 'YYYY/MM/DD HH24:MI:SS' ) >= '2019/03/21 12:31:55' 
-                        AND SUBSTR(TO_CHAR(CONTENU_ISO),0,3) = '501' 
+            HistoricoTags = db.Historico.OrderByDescending(x => x.Fecha).FirstOrDefault().Fecha;
+        }
+        public DateTime SendHistoricoTags()
+        {
+            return HistoricoTags;
+        }
+        public void GetOracleTransactions()
+        {
+            var query = @"Select count(*) From Transaction Where  ID_PAIEMENT = '15' 
+                        AND TO_CHAR(DATE_TRANSACTION, 'YYYY/MM/DD HH24:MI:SS' ) >= '2019/03/21 12:31:55'  
+                        AND SUBSTR(TO_CHAR(CONTENU_ISO),0,3) = '501'  
                         AND TAB_ID_CLASSE >= 1 order by DATE_TRANSACTION ASC";
             OracleConnection connection = new OracleConnection("User Id = GEADBA; Password = fgeuorjvne; Data Source = (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 10.1.10.111)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = GEAPROD)))");
+            connection.Open();
             OracleCommand cmd = new OracleCommand(query, connection);
-            OracleTransactions = cmd.ExecuteNonQuery();
+            OracleTransactions = Convert.ToInt32(cmd.ExecuteScalar());
+            connection.Close();
         }
         public int SendOracleTransactions()
         {
             return OracleTransactions;
         }
-        public async void GetSQLTransactions()
+        public void GetSQLTransactions()
         {
             SQLTransactions = db.Historico.Count();
         }
